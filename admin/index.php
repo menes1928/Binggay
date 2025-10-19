2b91489e5ac68ecd55fa3b0674890bc21c128edc
cxrstar <cxyris0419@gmail.com>
2025-10-20 01:37:08 +0800

    ppp

--- admin/index.php
+++ admin/index.php
@@ -3,14 +3,14 @@
 if (session_status() !== PHP_SESSION_ACTIVE) { session_start(); }
 $uid = isset($_SESSION['user_id']) ? (int)$_SESSION['user_id'] : 0;
 if ($uid <= 0) {
-    header('Location: ../index');
+    header('Location: ../index.php');
     exit;
 }
 $utype = isset($_SESSION['user_type']) ? (int)$_SESSION['user_type'] : 0;
 if ($utype !== 1) {
-    header('Location: ../user/index.php');
+    header('Location: ../user/home.php');
     exit;
 }
 // Admin OK: send to admin dashboard
-header('Location: ./admin');
+header('Location: ./admin.php');
 exit;
