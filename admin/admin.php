d40d01999d69ca9b2eaacaca27980e372e18a3b6
Brian Maines <phenominalgamer19@gmail.com>
2025-10-20 01:06:22 +0800

    .

--- admin/admin.php
+++ admin/admin.php
@@ -4,12 +4,12 @@ session_start();
 
 $uid = isset($_SESSION['user_id']) ? (int)$_SESSION['user_id'] : 0;
 if ($uid <= 0) {
-    header('Location: ../home');
+    header('Location: ../index');
     exit;
 }
 $utype = isset($_SESSION['user_type']) ? (int)$_SESSION['user_type'] : 0;
 if ($utype !== 1) {
-    header('Location: ../user/home');
+    header('Location: ../user/index');
     exit;
 }
 
